9d34f84dddb8a47ab8dcf3c9984bdf68d45ffc77
imabot05 <khanh8a04clone@gmail.com>
2024-11-06 14:10:34 +0700

    Update 20201_Final_Ex2.cpp

--- 20201_Final_Ex2.cpp
+++ 20201_Final_Ex2.cpp
@@ -6,27 +6,16 @@ int t, n;
 int a[MAX];
 
 int solve() {
-    vector<int> lis;  // Vector to store the potential LIS elements with consecutive increasing by 1
+    map<int, int> length_map;
+    int longest = 0;  
 
     for (int i = 0; i < n; i++) {
-        if (lis.empty() || (a[i] == lis.back() + 1)) {
-            // If lis is empty or a[i] is exactly 1 more than the last element in lis
-            lis.push_back(a[i]);
-        } else {
-            // Otherwise, find the position to replace in lis array using lower_bound
-            auto it = lower_bound(lis.begin(), lis.end(), a[i]);
-            
-            if (it == lis.end()) {
-                // If a[i] is greater than all elements, add it to the end
-                lis.push_back(a[i]);
-            } else {
-                // Replace the found position with a[i]
-                *it = a[i];
-            }
-        }
+        int current_length = length_map[a[i] - 1] + 1;
+        length_map[a[i]] = max(length_map[a[i]], current_length);
+        longest = max(longest, length_map[a[i]]);
     }
-    
-    return lis.size();  // The length of the vector lis is the length of the required LIS
+
+    return longest;
 }
 
 int main() {
